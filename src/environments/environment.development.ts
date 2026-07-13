8d83c98fd47124804d3155231e9e864ba1104a69
Gionx03 <sammarrogiovanni@gmail.com>
2026-07-13 17:25:19 +0200

    setup enviroment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+
+  apiBaseUrl: 'http://localhost:8080',
+
+  keycloak: {
+    url: 'http://localhost:8081',
+    realm: 'OrdinaMii',
+    clientId: 'ordinamii-frontend',
+  },
+};
